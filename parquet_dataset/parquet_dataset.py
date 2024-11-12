3967b7456ecd43e83f7e62a49db690c181fe2271
Bradley <bccollicott@gmail.com>
2024-11-11 22:44:25 -0800

    add a dataset that reads all of the rows from the parquet at a given date/time. broken currently because I try to concatenate unequal size tensors. also the previous date/time doesn't necessarily contain the same symbols, so I probably need to actually do something more clever to get the symbols + the previous responders for that symbol.

--- parquet_dataset/parquet_dataset.py
+++ parquet_dataset/parquet_dataset.py
@@ -77,7 +77,7 @@ class ParquetDataset(Dataset):
             for i, file in enumerate(file_paths):
                 print(f"\t{self.file_row_counts[i]} : {file}")
 
-            print(f"{len(self)} total samples.")
+            print(f"{self.total_num_rows} total rows.")
 
     def __len__(self) -> int:
         return self.total_num_rows
@@ -163,3 +163,123 @@ class ParquetDataset(Dataset):
         # Simple data cleaning.
         # TODO: Replace with a better strategy for handling missing data.
         self.pq_cache.df = self.pq_cache.df.fill_nan(0.0).fill_null(0.0)
+
+
+@dataclass
+class DateTimeSegment:
+    # List of file/rowgroup segment ids for this unique datetime
+    date: int
+    time: int
+    start_offset: int
+    end_offset: int
+    start_idx: int
+    end_idx: int
+
+
+class DatetimeParquetDataset(ParquetDataset):
+    """Parquet dataset where each sample is a unique date+time with multiple rows (corresponding to
+    multiple symbols). The samples will also include features+lags for the time_context_window
+    previous date+time samples."""
+
+    def __init__(self, file_paths: List[str], time_context_length: int = 1, logging=True):
+        super().__init__(file_paths=file_paths, logging=logging)
+
+        self.time_context_length = time_context_length
+
+        # Go through all of the parquet files again and store the start row for each date+time.
+        self.date_time_segments: List[DateTimeSegment] = []
+
+        # Iterate forward through the Parquet files and partially fill the DateTimeSegments.
+        self._populate_partial_dt_segments()
+
+        # Iterate through all the partial DateTimeSegments and finish populating
+        # them with offsets and segment indices.
+        self._finalize_dt_segments()
+
+    def _populate_partial_dt_segments(self) -> None:
+        prev_date, prev_time = None, None
+        for file_idx, file in enumerate(self.file_paths):
+
+            # load the ith file and get just the date and time columns
+            pq_df: pl.DataFrame = pl.read_parquet(file).select(["date_id", "time_id"])
+
+            # overall row where this file starts
+            start_row = self.cum_row_counts[file_idx]
+
+            for row_idx in range(len(pq_df)):
+                date, time = pq_df.row(row_idx)
+
+                if prev_date is None or prev_time is None or date != prev_date or time != prev_time:
+                    # Create a segment at the start of the new date+time
+                    segment = DateTimeSegment(
+                        date=date,
+                        time=time,
+                        start_offset=start_row + row_idx,
+                        end_offset=None,
+                        start_idx=None,
+                        end_idx=None,
+                    )
+                    self.date_time_segments.append(segment)
+
+                    prev_date = date
+                    prev_time = time
+
+    def _finalize_dt_segments(self) -> None:
+        # get the start and end segments
+        for i in range(len(self.date_time_segments) - 1):
+            seg_1, seg_2 = self.date_time_segments[i], self.date_time_segments[i + 1]
+            start_idx = self.calculate_index_from_cumulative_counts(
+                seg_1.start_offset, self.cum_total_counts_np
+            )
+            end_idx = self.calculate_index_from_cumulative_counts(
+                seg_2.start_offset - 1, self.cum_total_counts_np
+            )
+
+            seg_1.end_offset = seg_2.start_offset
+            seg_1.start_idx = start_idx
+            seg_1.end_idx = end_idx
+
+        final_segment = self.date_time_segments[-1]
+        final_segment.start_idx = self.calculate_index_from_cumulative_counts(
+            final_segment.start_offset, self.cum_total_counts_np
+        )
+        final_segment.end_idx = len(self.cum_total_counts)
+        final_segment.end_offset = self.total_num_rows
+
+    def __len__(self) -> int:
+        return len(self.date_time_segments)
+
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        return torch.stack(
+            [self._get_dt_sample(i) for i in range(max(0, idx - self.time_context_length), idx + 1)]
+        )
+
+    def _get_dt_sample(self, idx: int) -> torch.Tensor:
+        seg = self.date_time_segments[idx]
+        df_list: List[torch.Tensor] = []
+        for total_idx in range(seg.start_idx, seg.end_idx + 1):
+            group: RowGroupOffset = self.cum_total_counts[total_idx]
+            self._load_pq_file(
+                file_idx=group.file_idx,
+                row_group_idx=group.row_group_idx,
+            )
+
+            # start offset within this group is the total start offset minus the group start offset.
+            # If it's greater than zero, we need to discard some rows from the start of this row
+            # group.
+            start_offset = max(seg.start_offset - group.offset, 0)
+
+            # end offset within this group is the smaller of the total end row - the group start row
+            # and the size of the group. Basically if the end offset is larger than row group, just
+            # take the whole group. Otherwise we'll discard some rows from the end of this row
+            # group.
+            end_offset = min(
+                seg.end_offset - group.offset,
+                self.file_row_group_counts[group.file_idx][group.row_group_idx],
+            )
+
+            # The start row offset is ahead of the starting point for this row group.
+            # Need to select the rows starting at the offset.
+            df_list.append(self.pq_cache.df.to_torch()[start_offset:end_offset])
+
+        return torch.vstack(df_list)
